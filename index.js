8f123d5607b40d238793811f3b0f8adccc7b4c3b
Civoremo <omerovic_n@hotmail.com>
2019-01-10 15:06:18 -0500

    fixed port issue for heroku

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const tagServer = require('./api/tagServer.js');
 server.use(postServer);
 server.use(tagServer);
 
-server.listen(5000, () => console.log('server on port 5k'));
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log('server on port 5k'));
